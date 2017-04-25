3e19757fe58a9314f33eb6d976df1cc5e4437322
Paul Brattain <arachane@gmail.com>
2017-04-24 17:00:51 -0700

    Fixed PickedColor string generation

--- colorgame.js
+++ colorgame.js
@@ -66,7 +66,7 @@ for (var i=0; i < squares.length; i++){
 // add click listeners to squares
   squares[i].addEventListener("click", function(){
     // grab color of clicked squares
-    var clickedColor = this.style.background;
+     clickedColor = this.style.background;
     // compare color to pickedColor
 
     if(clickedColor === pickedColor){
@@ -113,5 +113,5 @@ function randomColor(){
   var g =Math.floor(Math.random() * 256);
     //Pick a "blue" from 0-255
   var b =Math.floor(Math.random() * 256);
-return "RGB(" + r + ", " +g + ", " +b + ")";
+return "rgb(" + r + ", " +g + ", " +b + ")";
 }
